c8280ce5cda1a4b018fd1e6fc638ee17eb952aec
uniysfu <uniysfu@gmail.com>
2016-02-09 19:50:47 +1100

    Update Consumer.java

--- Consumer.java
+++ Consumer.java
@@ -1,30 +1,80 @@
 package PIRM15S2;
 
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.ArrayList;
 
-public class PIRM {
-	static boolean keep = true;
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		/*
-		Scanner inpath = new Scanner(System.in);
-		String foodpath = inpath.next();
-		String userpath = inpath.next();
-		String inspath = inpath.next();
-		String logspath = inpath.next();
-		inpath.close();
-		*/
-		String foodpath = args[0];
-		String userpath = args[1];
-		String instructionpath = args[2];
-		String logspath = args[3];
+public class Consumer {
+	public String date;
+	public String foodtype;
+	public String consumername;
+	public double quantity;
+	public double eatcost;
+	
+	
+	public Consumer(String food , String name, String time, double count, double totalcost){
 		
-		Function.InstructionInput(foodpath, userpath, instructionpath, logspath);
-		/*
-		for(String s :Function.historyinsgroup){
-			System.out.println(s);
-		}
-		*/
+		foodtype = food;
+		consumername = name;
+		date = time;
+		quantity = count;
+		eatcost = totalcost;
+		
+		
 	}
+	
+	
+	public static void Count (ArrayList<Consumer> a){
+		
+		int n = a.size();
+		
+		System.out.println("elements of this arryalist is : "+n);
+		
+	}
+	
+	public void ShowValues(){
+		
+		System.out.println("$"+eatcost+" "+foodtype+" consumed by "+ consumername +" in "+date );
+	}
+	
+	
+	public Consumer(Consumer a){
+		this.foodtype = a.foodtype;
+		this.consumername = a.consumername;
+		this.date = a.date;
+		this.quantity = a.quantity;
+		this.eatcost =a.eatcost;
+	}
+	
+	public static void copy(Consumer a, Consumer b){
+		
+		a.foodtype = b.foodtype;
+		a.consumername = b.consumername;
+		a.date = b.date;
+		a.quantity = b.quantity;
+		a.eatcost =b.eatcost;
+		
+	}
+	
+	public static void AscendingSort(ArrayList <Consumer> sample){
+		if(sample.size() ==0) { return;
+		}
+		else{
+			// already add size ==1 condition
+			if(sample.size() ==1){ return;
+				
+			}
+			else{
+				for(int i = sample.size(); i>1; i--){
+					for(int j = 0; j<i-1; j++){
+						if(! Function.DateCompare(sample.get(j).date,sample.get(j+1).date)){
+							Consumer temp = new Consumer(sample.get(j+1));
+							Consumer.copy(sample.get(j+1), sample.get(j));
+							Consumer.copy(sample.get(j), temp);
+						}	
+					}
+				}
+			}
+		}
+		
+	}
+
 }
